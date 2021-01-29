3e6e03e07680d03c1a260361ec984339bacaed2c
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-01-29 14:11:32 -0600

    first commit

---
+++ Grocery List ChallengeLPB/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grocery_List_ChallengeLPB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        //DON'T FORGET TO USE METHODS
+
+        //Show number of items stored code
+
+        //Clear text boxes after every item
+
+        //Print out grocery list
+        private void itemAmountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addItemButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Make array(s)
+        string[] tempArray1 = new string[10];
+        string[] tempArray2 = new string[10];
+        private void GetNumberOfItems()
+        {
+
+        }
+        private void NumberOfItems()
+        {
+
+        }
+
+        private void GetItemPrices()
+        {
+
+        }
+
+
+
+    }
+}
